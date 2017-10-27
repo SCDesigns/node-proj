71cb47c1830e8400a004bb2baa19fccd7e109757
Sean Clarke <xseanclarke@gmail.com>
2017-10-27 16:34:30 -0400

    createServer now async + console feedback

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
-var http = require("http");
+var http = require("http"); // Requires Node's built in HTTP module and makes it accessible through the var http
 
-http.createServer(function(request, response) {
+/* We then call one of the functions the http module offers: createServer. This returns an
+object w/ a method named listen. It takes a numeric value which indicates the
+port number our HTTP server is going to listen on. */
+var http = require("http");
+function onRequest(request, response) {
+  console.log("Request received.");
   response.writeHead(200, {"Content-Type": "text/plain"});
   response.write("Hello World");
   response.end();
-}).listen(8888);
+}
+
+http.createServer(onRequest).listen(8888);
+console.log("Server has started.");
