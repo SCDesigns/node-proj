95cd7f7055be6bf58df2865cef13fd5b225bfe3d
Sean Clarke <xseanclarke@gmail.com>
2017-10-27 17:13:11 -0400

    created start() module w/ export

--- server.js
+++ server.js
@@ -3,13 +3,18 @@ var http = require("http"); // Requires Node's built in HTTP module and makes it
 /* We then call one of the functions the http module offers: createServer. This returns an
 object w/ a method named listen. It takes a numeric value which indicates the
 port number our HTTP server is going to listen on. */
-var http = require("http");
-function onRequest(request, response) {
-  console.log("Request received.");
-  response.writeHead(200, {"Content-Type": "text/plain"});
-  response.write("Hello World");
-  response.end();
+function start(){
+  function onRequest(request, response) {
+    console.log("Request received."); // This is printed twice upon navigating to localhost:8888 b/c most browsers attempt to load the favicon
+    response.writeHead(200, {"Content-Type": "text/plain"});
+    response.write("Hello World");
+    response.end();
+  }
+
+  http.createServer(onRequest).listen(8888);
+  console.log("Server has started.");
 }
 
-http.createServer(onRequest).listen(8888);
-console.log("Server has started.");
+exports.start = start;
+
+// by wrapping the server in the start() function and exporting it we can access it elsewhere, ex: index.js
