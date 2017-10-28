fb14931ce475098dac1e9d8009975b26fb9fad50
Sean Clarke <xseanclarke@gmail.com>
2017-10-27 21:50:38 -0400

    server.js now handling server 'handle' objects and routing by name

--- index.js
+++ index.js
@@ -8,12 +8,3 @@ handle["/start"] = requestHandlers.start;
 handle["/upload"] = requestHandlers.upload;
 
 server.start(router.route, handle);
-
-/*
-Just like any internal module:
-by requiring our server module's file and assigning
-it to a variable, its exported functions become
-available to us.
-
-we now start the app w/ node index.js
-*/
