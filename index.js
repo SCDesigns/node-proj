86878b9e8f28e71c28959f86af3b8d3551e31d57
Sean Clarke <xseanclarke@gmail.com>
2017-10-27 21:39:11 -0400

    created handle object to handle route requests

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 var server = require("./server");
 var router = require("./router");
+var requestHandlers = require("./requestHandlers");
 
-server.start(router.route);
+var handle = {}
+handle["/"] = requestHandlers.start;
+handle["/start"] = requestHandlers.start;
+handle["/upload"] = requestHandlers.upload;
+
+server.start(router.route, handle);
 
 /*
 Just like any internal module:
